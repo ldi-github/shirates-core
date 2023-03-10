c8fe075a0c267e5f0d2ba95c94aa081b5074f6f9
ldi_nsenba <n.senba@ldi.co.jp>
2023-03-11 02:16:42 +0900

    LanguageSettingsHelper add, remove, get, set

---
+++ src/test/kotlin/tutorial/basic/SetLanguageOnIos1.kt
@@ -0,0 +1,91 @@
+package tutorial.basic
+
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import shirates.core.configuration.Testrun
+import shirates.core.driver.befavior.LanguageSettingsHelper
+import shirates.core.driver.commandextension.*
+import shirates.core.testcode.UITest
+
+@Testrun("testConfig/ios/iOSSettings/testrun.properties")
+class SetLanguageOnIos1 : UITest() {
+
+    @Test
+    @Order(10)
+    fun setLanguage_getLanguage_removeLanguage1() {
+
+        scenario {
+            case(1) {
+                condition {
+                    LanguageSettingsHelper.gotoLocaleSettings()
+                }.action {
+                    s1 = LanguageSettingsHelper.setLanguage(language = "English", region = "Canada")
+                }.expectation {
+                    s1.thisIs("English (Canada)")
+                }
+            }
+            case(2) {
+                action {
+                    s1 = LanguageSettingsHelper.getLanguage()
+                }.expectation {
+                    s1.thisIs("English (Canada)")
+                }
+            }
+            case(3) {
+                action {
+                    LanguageSettingsHelper.removeLanguage(language = "English", region = "Canada")
+                }.expectation {
+                    it.dontExist("English (Canada)")
+                }
+            }
+            case(4) {
+                action {
+                    LanguageSettingsHelper.setLanguage(language = "English", region = "United States")
+                    s1 = LanguageSettingsHelper.getLanguage()
+                }.expectation {
+                    s1.thisIs("English (United States)")
+                }
+            }
+        }
+    }
+
+    @Test
+    @Order(20)
+    fun setLanguage_getLanguage_removeLanguage2() {
+
+        scenario {
+            case(1) {
+                condition {
+                    LanguageSettingsHelper.gotoLocaleSettings()
+                }.action {
+                    s1 = LanguageSettingsHelper.setLanguage(language = "日本語", region = "日本")
+                }.expectation {
+                    s1.thisIs("日本語 (日本)")
+                }
+            }
+            case(2) {
+                action {
+                    s1 = LanguageSettingsHelper.getLanguage()
+                }.expectation {
+                    s1.thisIs("日本語 (日本)")
+                }
+            }
+            case(3) {
+                action {
+                    LanguageSettingsHelper.removeLanguage(language = "日本語", region = "日本")
+                }.expectation {
+                    it.dontExist("日本語 (日本)")
+                }
+            }
+            case(4) {
+                action {
+                    LanguageSettingsHelper.setLanguage(language = "English", region = "United States")
+                    s1 = LanguageSettingsHelper.getLanguage()
+                }.expectation {
+                    s1.thisIs("English (United States)")
+                }
+            }
+        }
+    }
+
+}
