3b94f9165245150ed0bd461d3e7e672020d70bc3
ldi_nsenba <n.senba@ldi.co.jp>
2023-04-21 23:30:10 +0900

    4.3.1-SNAPSHOT

--- build/generated/source/buildConfig/main/main/io/github/ldi_github/shirates_core/BuildConfig.kt
+++ build/generated/source/buildConfig/main/main/io/github/ldi_github/shirates_core/BuildConfig.kt
@@ -5,7 +5,7 @@ import kotlin.String
 object BuildConfig {
     const val appName: String = "shirates-core"
 
-    const val version: String = "4.3.0"
+    const val version: String = "4.3.1-SNAPSHOT"
 
     const val packageName: String = "io.github.ldi-github"
 
