de33afcd9378ad8c0805d3e247e13aacedfd727b
ldi_nsenba <n.senba@ldi.co.jp>
2023-05-07 20:00:14 +0900

    Test Fix

--- src/test/kotlin/shirates/spec/uitest/SpecReportNoLoadRunTest.kt
+++ src/test/kotlin/shirates/spec/uitest/SpecReportNoLoadRunTest.kt
@@ -159,7 +159,7 @@ class SpecReportNoLoadRunTest : UITest() {
             testConfigName = "Settings",
             sheetName = "SheetName1",
             testClassName = "SpecReportNoLoadRunTest",
-            profileName = "Android 12",
+            profileName = "Pixel 3a(Android 12)",
             deviceModel = "",
             platformVersion = "",
             noLoadRunMode = "No-Load-Run Mode",
