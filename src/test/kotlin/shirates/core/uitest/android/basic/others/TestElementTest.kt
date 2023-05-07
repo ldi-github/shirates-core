2faa89b8c31ff6a84179596cbb3cd423eb8c90a4
ldi_nsenba <n.senba@ldi.co.jp>
2023-05-08 01:16:32 +0900

    Test Fix

--- src/test/kotlin/shirates/core/uitest/android/basic/others/TestElementTest.kt
+++ src/test/kotlin/shirates/core/uitest/android/basic/others/TestElementTest.kt
@@ -3,15 +3,12 @@ package shirates.core.uitest.android.basic.others
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import shirates.core.configuration.Selector
+import shirates.core.configuration.Testrun
 import shirates.core.driver.TestElement
 import shirates.core.testcode.UITest
 
+@Testrun("unitTestConfig/android/androidSettings/testrun.properties")
 class TestElementTest : UITest() {
-
-    override fun setup() {
-
-        setupFromTestrun("unitTestConfig/android/androidSettings/testrun.properties", "Android 12")
-    }
 
     @Test
     fun isEmpty() {
