079655465a250ce7e1c140b519107b9f57f5c377
ldi_nsenba <n.senba@ldi.co.jp>
2024-01-25 00:56:01 +0900

    6.5.2-SNAPSHOT

--- build/generated/source/buildConfig/main/main/io/github/ldi_github/shirates_core/BuildConfig.kt
+++ build/generated/source/buildConfig/main/main/io/github/ldi_github/shirates_core/BuildConfig.kt
@@ -5,7 +5,7 @@ import kotlin.String
 object BuildConfig {
     const val appName: String = "shirates-core"
 
-    const val version: String = "6.5.1"
+    const val version: String = "6.5.2-SNAPSHOT"
 
     const val packageName: String = "io.github.ldi-github"
 
