c7574f268cf77699b3c532133217da56ab4bf2d3
ldi_nsenba <n.senba@ldi.co.jp>
2024-03-13 02:51:34 +0900

    6.7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "io.github.ldi-github"
-version = "6.7.2-SNAPSHOT"
+version = "6.7.2"
 
 val appiumClientVersion = "9.1.0"
 
--- build/generated/source/buildConfig/main/main/io/github/ldi_github/shirates_core/BuildConfig.kt
+++ build/generated/source/buildConfig/main/main/io/github/ldi_github/shirates_core/BuildConfig.kt
@@ -5,7 +5,7 @@ import kotlin.String
 object BuildConfig {
     const val appName: String = "shirates-core"
 
-    const val version: String = "6.7.2-SNAPSHOT"
+    const val version: String = "6.7.2"
 
     const val packageName: String = "io.github.ldi-github"
 
