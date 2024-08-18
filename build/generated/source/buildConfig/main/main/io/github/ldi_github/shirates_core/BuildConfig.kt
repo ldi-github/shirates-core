7d5f04185efdd4f3bf9f5fe7b9820aa75b0ed893
ldi_nsenba <n.senba@ldi.co.jp>
2024-08-19 00:43:02 +0900

    7.5.3

--- build/generated/source/buildConfig/main/main/io/github/ldi_github/shirates_core/BuildConfig.kt
+++ build/generated/source/buildConfig/main/main/io/github/ldi_github/shirates_core/BuildConfig.kt
@@ -5,7 +5,7 @@ import kotlin.String
 object BuildConfig {
     const val appName: String = "shirates-core"
 
-    const val version: String = "7.5.3-SNAPSHOT"
+    const val version: String = "7.5.3"
 
     const val packageName: String = "io.github.ldi-github"
 
